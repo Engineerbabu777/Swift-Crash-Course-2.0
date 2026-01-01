4a16ce944da3b4518c147205686741418a6bc7fe
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 20:54:12 +0500

    statements done

---
+++ statements/main.swift
@@ -0,0 +1,13 @@
+
+
+let x = 2  // declaration
+print(x)    // expression statement
+
+
+let a = 1; let b = 2 // semicolons can separate multiple statements on one line
+print(a + b)
+
+
+if a < b {
+  print("a is less than b")
+}
