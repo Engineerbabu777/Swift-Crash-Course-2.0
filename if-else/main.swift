46cce08d67dbd053110d013fc90aa866166300cf
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-02 11:40:48 +0500

    add(if-else): done

---
+++ if-else/main.swift
@@ -0,0 +1,58 @@
+
+
+// let score = 82
+// if score >= 90 {
+//   print("A")
+// } else if score >= 80 {
+//   print("B")
+// } else {
+//   print("C or lower")
+// }
+
+
+let temp = 25
+if temp > 20 {
+  print("Warm")
+}
+
+let hasAccess = false
+if hasAccess {
+  print("Welcome")
+} else {
+  print("Denied")
+}
+
+
+let score = 72
+if score >= 90 {
+  print("A")
+} else if score >= 80 {
+  print("B")
+} else if score >= 70 {
+  print("C")
+} else {
+  print("Below C")
+}
+
+
+let t = 0
+if t <= 0 {
+  print("Freezing")
+} else if t < 10 {
+  print("Cold")
+} else if t < 20 {
+  print("Cool")
+} else {
+  print("Warm")
+}
+
+
+let isLoggedIn = true
+let isAdmin = false
+if isLoggedIn {
+    if isAdmin {
+      print("Admin panel")
+    } else {
+      print("Limited access")
+    }
+  }
