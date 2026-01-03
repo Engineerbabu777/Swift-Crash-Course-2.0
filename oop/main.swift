b07d32852f485a345827766e1fe080e3aca9a934
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-03 15:57:25 +0500

    statis method and class methods

--- oop/main.swift
+++ oop/main.swift
@@ -72,3 +72,18 @@ class Player {
 
 let p = Player()
 p.score = 10
+
+
+class Base {
+  class func greet() { print("Base") }
+  static func ping() { print("Base ping") }
+}
+
+class Sub: Base {
+  override class func greet() { print("Sub") }
+  // static func cannot be overridden
+}
+
+Base.greet()
+Sub.greet()
+Base.ping()
