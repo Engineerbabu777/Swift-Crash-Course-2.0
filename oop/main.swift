5b2e795b4efdb78d0687e9bed3755e582e9eb78b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-03 15:54:50 +0500

    propertries observer and static properties done

--- oop/main.swift
+++ oop/main.swift
@@ -30,3 +30,45 @@ class BankAccount {
 let account = BankAccount(10)
 account.deposit(5)
 print(account.checkBalance()) // prints "5"
+
+
+
+struct Point { var x: Int; var y: Int }
+var p1 = Point(x: 1, y: 2)
+var p2 = p1 // copy
+p2.x = 10
+print(p1.x) // prints "1"
+print(p2.x) // prints "10"
+
+
+
+// struct Point { var x: Int; var y: Int }
+// class Counter { var value = 0 }
+
+// var p1 = Point(x: 1, y: 2), p2 = p1 // copy
+// p2.x = 9 // p1.x remains 1
+
+// let c1 = Counter(), c2 = c1 // same instance
+// c2.value = 7 // c1.value is 7
+
+
+// static
+
+class User {
+  var name: String // stored
+  var greeting: String { "Hello, \(name)" } // computed
+  static var appName = "MyApp" // type property
+  init(name: String) { self.name = name }
+}
+
+
+
+class Player {
+  var score: Int = 0 {
+    willSet { print("About to set to \(newValue)") }
+    didSet { print("Changed from \(oldValue) to \(score)") }
+  }
+}
+
+let p = Player()
+p.score = 10
