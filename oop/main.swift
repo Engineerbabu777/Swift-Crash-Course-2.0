1a3816407a7c1e4b32e4ef3d276ba8082f4dc8db
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-03 15:48:33 +0500

    class with construtor and overloading constructor and setter and getter fnc done

--- oop/main.swift
+++ oop/main.swift
@@ -1,7 +1,5 @@
-
-
 class Counter {
-   var value = 0
+  var value = 0
   func inc() { value += 1 }
 }
 
@@ -9,3 +7,26 @@ let c = Counter()
 c.inc()
 
 // print(c.value) // prints "1"
+
+class BankAccount {
+  var balance: Int
+
+  init() {
+    self.balance = 0
+  }
+  init(_ balance: Int) {
+    self.balance = balance
+  }
+
+  func deposit(_ amount: Int) {
+    balance += amount
+  }
+
+  func checkBalance() -> Int {
+    return balance
+  }
+}
+
+let account = BankAccount(10)
+account.deposit(5)
+print(account.checkBalance()) // prints "5"
